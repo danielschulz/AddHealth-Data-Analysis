f48fc59ab06b89eb74a282d7ec506fad828f2050
Daniel Schulz <danielschulz2005@hotmail.com>
2013-05-14 22:11:16 +0200

    init religious biases

--- output/scripts/ReligiousBiases.R
+++ output/scripts/ReligiousBiases.R
@@ -1,2 +1,11 @@
 # clean
 rm(list=ls())
+
+setWD = tryCatch({
+  # set working directory
+  setwd(paste(getwd(), "/output", sep=""))
+}, error = function(e) {})
+
+# load data
+relativeAddHealthPath = "../input/data/addhealth_pds3.RData"
+load(relativeAddHealthPath)
